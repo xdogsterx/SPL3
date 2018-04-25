2c6a98ec3580ecb8809b96457061027022db0134
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-25 10:25:36 +0200

    Fertiges Programm

--- parkhaus.py
+++ parkhaus.py
@@ -3,17 +3,33 @@
 
 print("Linienbus-Simulator")
 
+personenMax = 60
+personen = 0
 haltestellen = input("Wie viele Haltestellen gibt es?")
 haltestellenAusgabe = int(haltestellen)
 
 for h in range (1,haltestellenAusgabe+1):
-    print("Der Bus fährt.")
-    print("Der Bus steht an Haltestelle ", h, ".") 
-    einsteiger = input("Wie viele Personen steigen ein?")
-    personen = int(einsteiger)   
-    aussteiger = input("Wie viele Personen möchten aussteigen?")
-    aussteigerAusgabe = int(aussteiger)
-    print("Es sind ", personen-aussteigerAusgabe, " Personen im Bus.")
+    personenOk = False
+    while(personenOk == False):
+        print("Der Bus steht an Haltestelle ", h, ".") 
+        einsteiger = input("Wie viele Personen steigen ein?")
+        personen = personen + int(einsteiger)   
+        aussteiger = input("Wie viele Personen möchten aussteigen?")
+        personen = personen-int(aussteiger)
+        aussteigerAusgabe = int(aussteiger)
+        insgesammtLeute = personen-aussteigerAusgabe
+        personenImBus = personen
+        print("Es sind ", personenImBus, " Personen im Bus.")
+        if(personenImBus<=personenMax and personenImBus > 0):
+            print("Der Bus fährt.")
+            personenOk = True
+        elif(personenImBus<0):
+            print("Fehler! Negativer Wert erkannt! Es würden sich ", personen, " Personen im Bus befinden!")
+            personen = personen+int(aussteiger)
+            personen = personen-int(einsteiger)
+            print("Es sind ", personen, " Personen im Bus.")
+        else:
+            print("Es können", personen-personenMax, "Personen nicht mitfahren")
 
 print("Der Bus hat die Endstation erreicht.")
 print("Bitte alle aussteigen!")
