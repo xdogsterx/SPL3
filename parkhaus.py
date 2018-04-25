ac71a20274fc4cd097c01b4279924d1ccf5a75c5
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-25 08:59:40 +0200

    Ausgabe der Anzahl an Personen im Bus

--- parkhaus.py
+++ parkhaus.py
@@ -1,2 +1,15 @@
 # parkhaus.py 
 # Angabe für das Beispiel: siehe moodle
+
+print("Linienbus-Simulator")
+
+haltestellen = input("Wie viele Haltestellen gibt es?")
+
+print("Der Bus steht an Haltestelle ", haltestellen)
+
+einsteiger = input("Wie viele Personen steigen ein?")
+personen = int(einsteiger)
+
+for h in range (0,10):
+    
+    print("Es sind ", personen, " Personen im Bus.")
