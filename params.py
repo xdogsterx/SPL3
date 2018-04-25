6239b3f482b7280a7541fdf397738ae7a2c06887
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-25 12:22:54 +0200

    updated

---
+++ params.py
@@ -0,0 +1,12 @@
+# params.py 
+import sys
+#z1=int(input("Zahl 1: ")
+#z2=int(input("Zahl 2: ")
+#z3=int(input("Zahl 3: ")
+
+p = sys.argv
+z1 = int(p[1])
+z2 = int(p[2])
+z3 = int(p[3])
+
+print(z1+z2+z3)
