3e1082373a0110e3be8b8dd74c6493b85f7315ee
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-25 10:42:01 +0200

    zeitberechnung!

--- zeiten.py
+++ zeiten.py
@@ -1,1 +1,19 @@
 # zeiten.py 
+
+beginnZeit = input("Beginnzeit: ")
+##endeZeit = input("Endezeit: ")
+
+# Zeit in Format HH:MM:SS
+beginn = beginnZeit.split(":")
+print (beginn)
+
+h = int(beginn[0])
+m = int(beginn[1])
+s = int(beginn[2])
+
+print("Stunden: ", h)
+print("Minuten: ", m)
+print("Sekunden: ", s)
+
+beginnSekunden = s + m*60 + h*3600
+print("Gesamt Sekunden Beginn: ", beginnSekunden)
