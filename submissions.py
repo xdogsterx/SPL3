da77019ad57ebf7d123f6c1419f7e770937c6fdd
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-25 12:07:09 +0200

    fertigstellung

--- submissions.py
+++ submissions.py
@@ -2,11 +2,19 @@
 
 abgabe = "10:00:00 3 1 10:20:00 wrong 1 10:45:00 correct 2 10:20:01 correct"
 t = abgabe.split(" ")
-print(t)
+
 startzeit = t[0]
 submissions = t[1]
+besteZeit = "23:59:59"
+besterUser = -1
 
-for x in range(2, len(t), 3):
-    print("UserID", t[x])
-    print("Zeit", t[x+1])
-    print("Lösung", t[x+2])
+for position in range(2, len(t), 3):
+    userId = t[position]
+    zeit = t[position+1]
+    loesung = t[position+2]
+    print("Abgabe: ", userId, zeit, loesung)
+    if(zeit < besteZeit and loesung == "correct"):
+        besteZeit = zeit
+        besterUser = userId
+
+print(besterUser, besteZeit)
