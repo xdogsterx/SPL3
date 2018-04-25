2f059555caa33fdb55717335599fcb9e996cdeaf
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-25 10:42:57 +0200

    zeitberechnung2

--- zeiten.py
+++ zeiten.py
@@ -1,5 +1,12 @@
 # zeiten.py 
 
+def zeitInSekunden(h,m,s):
+    gesamt = 0
+    gesamt += h * 3600
+    gesamt += m * 60
+    gesamt += s
+    return gesamt
+
 beginnZeit = input("Beginnzeit: ")
 ##endeZeit = input("Endezeit: ")
 
