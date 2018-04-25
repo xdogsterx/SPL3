c58b2472a6f1955f3c6fb4673782949baf61a4ec
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-25 09:23:12 +0200

    Erweiterung zum Aussteigen

--- parkhaus.py
+++ parkhaus.py
@@ -4,13 +4,16 @@
 print("Linienbus-Simulator")
 
 haltestellen = input("Wie viele Haltestellen gibt es?")
- 
+haltestellenAusgabe = int(haltestellen)
 
-for h in range (1,haltestellen):
+for h in range (1,haltestellenAusgabe+1):
+    print("Der Bus fährt.")
     print("Der Bus steht an Haltestelle ", h, ".") 
     einsteiger = input("Wie viele Personen steigen ein?")
     personen = int(einsteiger)   
-    print("Es sind ", personen, " Personen im Bus.")
+    aussteiger = input("Wie viele Personen möchten aussteigen?")
+    aussteigerAusgabe = int(aussteiger)
+    print("Es sind ", personen-aussteigerAusgabe, " Personen im Bus.")
 
 print("Der Bus hat die Endstation erreicht.")
 print("Bitte alle aussteigen!")
